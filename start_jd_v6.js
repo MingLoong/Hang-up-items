467c3d3a93b6db1e27b752d1cf1301f5266356ab
spiritlhl <62994705+spiritLHL@users.noreply.github.com>
2021-10-22 12:03:57 +0800

    Update and rename start_jd_v5.js to start_jd_v6.js
    更新

--- start_jd_v6.js
+++ start_jd_v6.js
@@ -1,12 +1,14 @@
 // https://github.com/spiritLHL/Hang-up-items/edit/master/start_jd_v5.js
 // 先自己开无障碍模式和允许浮窗显示，再运行脚本
 // 这是未完善脚本，如果有的任务已经做了，该脚本无法执行
+// 本脚本需手动进入活动界面，且打开任务列表才可起效
+// 针对<玩家分赃>活动进行调整 20211021
 if (!auto.service) {
     toast('无障碍服务未启动！退出！')
     exit()
 }
 
-alert('即将请求截图权限，用以查找按钮，请允许')
+// alert('即将请求截图权限，用以查找按钮，请允许')
 if (!requestScreenCapture()) {
     console.log("请求截图失败，退出");
     exit();
@@ -32,62 +34,13 @@ function registerKey() {
 }
 threads.start(registerKey)
 
-// 打开京东
-console.log('正在打开京东App...')
-if (!launch('com.jingdong.app.mall')) {
-    console.log('未找到京东App，请先下载！')
-    exit()
+while (text("累计任务奖励").findOnce() == null) {
+    toast("请手动进入活动任务页面");
+    console.log('请手动进入活动任务页面')
+    sleep(2000);
 }
-sleep(2000)
 
-// 进入活动
-console.log('等待页面加载...')
-const into = descContains('浮层活动').findOne(20000)
 sleep(2000)
-if (into == null) {
-    console.log('无法找到活动入口，异常退出！')
-    exit()
-}
-click(into.bounds().centerX(), into.bounds().centerY())
-click(into.bounds().centerX(), into.bounds().centerY())
-console.log('进入活动页面')
-
-if (!textContains('每日签到抽最高').findOne(20000)) {
-    console.log('未能进入活动，请重新运行！')
-    exit()
-}
-// scrollDown()
-sleep(2000)
-// scrollUp()
-
-console.log('打开任务列表')
-let taskListButton = textMatches(/.*消耗.*汪汪币/).findOne(20000)
-if (!taskListButton) {
-    console.log('未能打开任务列表，请关闭京东重新运行！')
-    exit()
-}
-taskListButton = taskListButton.parent().children()
-let flag
-for (let i = 7; i < taskListButton.length; i++) {
-    let item = taskListButton[i]
-    if (item.text().match(/消耗.*汪汪币/)) {
-        flag = i
-        continue
-    }
-    if (flag) {
-        if (item.clickable()) {
-            taskListButton = item
-            break
-        }
-    }
-}
-if (!(taskListButton.clickable && taskListButton.clickable())) {
-    console.log('请在10s内手动打开任务列表,没空修,凑活用吧')
-    sleep(10000)
-}else{
-    taskListButton.click()
-    sleep(2000)
-}
 
 while (true) {
     function timeTask() {
@@ -153,9 +106,12 @@ while (true) {
     }
 
     console.log('寻找未完成任务...')
-    let taskButtons = textMatches(/.*浏览并关注.*|.*浏览.*s.*|.*累计浏览.*|.*浏览可得.*/).find()
+    let taskButtons = textMatches(/.*浏览并关注.*|.*浏览.*s.*|浏览.*s.*|.*累计浏览.*|.*浏览可得.*/).find();
     if (taskButtons.empty()) {
-        console.log('未找到浏览任务，退出')
+        console.log('未找到浏览任务，退出，城城分现金任务手动完成，如有未完成重新运行')
+        console.log('其它组队、浏览品牌墙、点击首页等莫名其妙任务自己搞定！！！')
+        sleep(5000)
+        console.hide()
         break
     }
 
@@ -176,7 +132,8 @@ while (true) {
 
     if (!taskButton) {
         console.log('未找到可自动完成的任务，退出。')
-        console.log('入会任务、互动任务需要手动完成。')
+        console.log('未找到浏览任务，退出，城城分现金任务手动完成，如有未完成重新运行')
+        console.log('其它组队、浏览品牌墙、点击首页、下单任务等莫名其妙任务自己搞定！！！')
         break
     }
 
@@ -197,24 +154,23 @@ while (true) {
             back()
             sleep(5000)
         } else {
-            sleep(2000)
-            check = check.parent().child(0).bounds()
-            console.log('即将勾选授权，自动隐藏控制台')
-            console.hide()
-            sleep(500)
-            click(check.centerX(), check.centerY())
-            sleep(500)
-            let j = text('确认授权并加入店铺会员').findOnce().bounds()
-            click(j.centerX(), j.centerY())
-            sleep(500)
-            console.show()
-            console.log('等待自动返回...')
-            if (!textContains('累计任务奖励').findOne(8000)) { // 部分任务会自动返回
-                console.log('手动返回')
-                back()
-            }
-            sleep(5000)
+        sleep(2000)
+        check = check.parent().child(0).bounds()
+        console.log('即将勾选授权，自动隐藏控制台')
+        console.hide()
+        sleep(500)
+        click(check.centerX(), check.centerY())
+        sleep(500)
+        let j = text('确认授权并加入店铺会员').findOnce().bounds()
+        click(j.centerX(), j.centerY())
+        sleep(500)
+        console.show()
+        console.log('等待自动返回...')
+        if (!textContains('累计任务奖励').findOne(8000)) { // 部分任务会自动返回
+            console.log('手动返回')
+            back()
         }
+        sleep(5000)}
     } else if (taskText.match(/浏览可得|浏览并关注/)) {
         let taskName = taskButton.parent().child(1).text()
         if (taskName.match(/种草城/)) {
